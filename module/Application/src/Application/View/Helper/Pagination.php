780be8835cb926d2c3c9b5dfd185eb21719d4b5b
Eric Lamb <eric@mithra62.com>
2016-01-17 11:40:09 -0800

    Updates so doesn't display unless has links

--- module/Application/src/Application/View/Helper/Pagination.php
+++ module/Application/src/Application/View/Helper/Pagination.php
@@ -30,28 +30,31 @@ class Pagination extends BaseViewHelper
      */
     public function __invoke($route_key, $total_pages = 1, $current_page = 1, array $options = array())
     {
-        $return = '<ul class="pagination pull-right">';
-        for($i=1;$i<=$total_pages;$i++) {
-            
-            if($i === 1){
-                $prev_page = $current_page-1;
-                if($total_pages >  $prev_page && $prev_page !== 0) {
-                    $return .= '<li><a href="'.$this->view->url($route_key).'?page='.$prev_page.'">&laquo;</a></li>';       
+        $return = '';
+        if( $total_pages > $current_page )
+        {
+            $return = '<ul class="pagination pull-right">';
+            for($i=1;$i<=$total_pages;$i++) {
+                
+                if($i === 1){
+                    $prev_page = $current_page-1;
+                    if($total_pages >  $prev_page && $prev_page !== 0) {
+                        $return .= '<li><a href="'.$this->view->url($route_key).'?page='.$prev_page.'">&laquo;</a></li>';       
+                    }
                 }
+                
+                $return .= '<li class="'.($i == $current_page ? 'active' : '').'"><a href="'.$this->view->url($route_key).'?page='.$i.'">'.$i.'</a></li>';
+                
+                if($i == $total_pages) {
+                    $next_page = $current_page+1;
+                    if( $next_page <= $total_pages ) {
+                        $return .= '<li><a href="'.$this->view->url($route_key).'?page='.$next_page.'">&raquo;</a></li>';
+                    }
+                }       
             }
             
-            $return .= '<li class="'.($i == $current_page ? 'active' : '').'"><a href="'.$this->view->url($route_key).'?page='.$i.'">'.$i.'</a></li>';
-            
-            if($i == $total_pages) {
-                $next_page = $current_page+1;
-                if( $next_page <= $total_pages ) {
-                    $return .= '<li><a href="'.$this->view->url($route_key).'?page='.$next_page.'">&raquo;</a></li>';
-                }
-            }       
+            $return .= '</ul>';
         }
-        
-        $return .= '</ul>';
-            
         return $return;            
     }
 }
